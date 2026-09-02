71422d245cf5456d8147bbd9d66d2221f4b46f0e
Tulis12 <agma116434@gmail.com>
2026-09-02 20:10:12 +0200

    update: configurable options for pdfs

--- app/src/main/java/dev/tulis/readbear/routes/menu/BottomSettingsSheet.kt
+++ app/src/main/java/dev/tulis/readbear/routes/menu/BottomSettingsSheet.kt
@@ -1,13 +1,20 @@
 package dev.tulis.readbear.routes.menu
 
 import androidx.compose.foundation.basicMarquee
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.PrimaryTabRow
@@ -17,38 +24,68 @@ import androidx.compose.material3.Slider
 import androidx.compose.material3.Tab
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.datastore.preferences.core.edit
+import coil3.compose.AsyncImage
 import dev.tulis.readbear.R
+import dev.tulis.readbear.db.Settings
+import dev.tulis.readbear.db.Settings.dataStore
 import dev.tulis.readbear.routes.menu.settings.AlreadyRead
+import dev.tulis.readbear.routes.menu.settings.PdfLayout
+import dev.tulis.readbear.utils.sampleImages
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun BottomSettingsSheet(
     sheetState: SheetState,
-    columnCount: Int,
-    onChangeColumnCount: (Int) -> Unit,
-    tooLongTextOption: TooLongTextOption,
-    onChangeTooLongTextOption: (TooLongTextOption) -> Unit,
-    alreadyReadOption: AlreadyReadOption,
-    onChangeAlreadyReadOption: (AlreadyReadOption) -> Unit,
-    onSaveRequest: () -> Unit,
     onHide: () -> Unit
 ) {
+    val scope = rememberCoroutineScope()
+    val context = LocalContext.current
+
+    val settingsFlow by Settings.getSettings(context).collectAsState(null)
+    val settings = settingsFlow ?: return
+
+    var columnCount by remember { mutableIntStateOf(settings.columnCount) }
+    var longTextOption by remember { mutableStateOf(settings.longTextOption) }
+    var alreadyReadOption by remember { mutableStateOf(settings.alreadyReadOption) }
+    var progressEnabled by remember { mutableStateOf(settings.progressEnabled) }
+    var timeClockEnabled by remember { mutableStateOf(settings.timeClockEnabled) }
+    var pdfReadingLayout by remember { mutableStateOf(settings.pdfReadingLayout) }
+
+    val onSaveRequest = {
+        scope.launch {
+            context.dataStore.edit { settings ->
+                settings[Settings.SettingsKeys.COLUMNS] = columnCount
+                settings[Settings.SettingsKeys.LONG_TEXT_OPTION] = longTextOption.name
+                settings[Settings.SettingsKeys.ALREADY_READ_OPTION] = alreadyReadOption.name
+                settings[Settings.SettingsKeys.PROGRESS_ENABLED] = progressEnabled
+                settings[Settings.SettingsKeys.TIME_CLOCK_ENABLED] = timeClockEnabled
+                settings[Settings.SettingsKeys.PDF_LAYOUT] = pdfReadingLayout.name
+            }
+        }
+    }
+
     ModalBottomSheet(
         sheetState = sheetState,
         onDismissRequest = {
-            onHide()
             onSaveRequest()
+            onHide()
         },
         modifier = Modifier
             .wrapContentHeight()
@@ -56,7 +93,8 @@ fun BottomSettingsSheet(
         var selectedTab by remember { mutableIntStateOf(0) }
 
         val tabs = listOf(
-            stringResource(R.string.library_settings)
+            stringResource(R.string.library_settings),
+            stringResource(R.string.pdf)
         )
 
         Column {
@@ -79,16 +117,33 @@ fun BottomSettingsSheet(
             when (selectedTab) {
                 0 -> LibrarySettings(
                     columnCount = columnCount,
-                    onChangeColumnCount = onChangeColumnCount,
-                    tooLongTextOption = tooLongTextOption,
-                    onChangeTooLongTextOption = onChangeTooLongTextOption,
+                    onChangeColumnCount = {
+                        columnCount = it
+                    },
+                    tooLongTextOption = longTextOption,
+                    onChangeTooLongTextOption = {
+                        longTextOption = it
+                    },
                     alreadyReadOption = alreadyReadOption,
-                    onChangeAlreadyReadOption = onChangeAlreadyReadOption,
+                    onChangeAlreadyReadOption = {
+                        alreadyReadOption = it
+                    },
+                    progressEnabled = progressEnabled,
+                    onChangeProgressEnabled = {
+                        progressEnabled = it
+                    },
+                    timeClockEnabled = timeClockEnabled,
+                    onChangeTimeClockEnabled = {
+                        timeClockEnabled = it
+                    }
                 )
 
-                1 -> {
-                    Text("uwu")
-                }
+                1 -> PdfReaderSettings(
+                    pdfReadingLayout = pdfReadingLayout,
+                    onChangePdfReadingLayout = {
+                        pdfReadingLayout = it
+                    }
+                )
             }
 
             Button(
@@ -105,17 +160,36 @@ fun BottomSettingsSheet(
 }
 
 @Composable
+fun PdfReaderSettings(
+    pdfReadingLayout: PdfReadingLayout,
+    onChangePdfReadingLayout: (PdfReadingLayout) -> Unit,
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    ) {
+        PdfLayout(pdfReadingLayout, onChangePdfReadingLayout)
+    }
+}
+
+@Composable
 fun LibrarySettings(
     columnCount: Int,
     onChangeColumnCount: (Int) -> Unit,
+
     tooLongTextOption: TooLongTextOption,
     onChangeTooLongTextOption: (TooLongTextOption) -> Unit,
-    alreadyReadOption: AlreadyReadOption,
-    onChangeAlreadyReadOption: (AlreadyReadOption) -> Unit
-) {
-    var sliderColumnValue by remember { mutableIntStateOf(columnCount) }
-    var tooLongTextOptionValue by remember { mutableStateOf(tooLongTextOption) }
 
+    alreadyReadOption: AlreadyReadOption,
+    onChangeAlreadyReadOption: (AlreadyReadOption) -> Unit,
+
+    progressEnabled: Boolean,
+    onChangeProgressEnabled: (Boolean) -> Unit,
+
+    timeClockEnabled: Boolean,
+    onChangeTimeClockEnabled: (Boolean) -> Unit
+) {
     Column(
         modifier = Modifier
             .fillMaxWidth()
@@ -128,14 +202,46 @@ fun LibrarySettings(
             Text(stringResource(R.string.column_count))
 
             Slider(
-                value = sliderColumnValue.toFloat(),
+                value = columnCount.toFloat(),
                 onValueChange = { value ->
                     onChangeColumnCount(value.toInt())
-                    sliderColumnValue = value.toInt()
                 },
                 valueRange = 1f..5f,
                 steps = 3
             )
+        }
+
+        Text(stringResource(R.string.show))
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(10.dp, Alignment.CenterHorizontally)
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Checkbox(
+                    checked = progressEnabled,
+                    onCheckedChange = {
+                        onChangeProgressEnabled(it)
+                    }
+                )
+
+                Text(stringResource(R.string.progress_enabled))
+            }
+
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Checkbox(
+                    checked = timeClockEnabled,
+                    onCheckedChange = {
+                        onChangeTimeClockEnabled(it)
+                    }
+                )
+
+                Text(stringResource(R.string.time_clock_enabled))
+            }
         }
 
         val longText = stringResource(R.string.this_is_a_long_text)
@@ -157,10 +263,9 @@ fun LibrarySettings(
                 )
 
                 RadioButton(
-                    selected = tooLongTextOptionValue == TooLongTextOption.BASIC_MARQUEE,
+                    selected = tooLongTextOption == TooLongTextOption.BASIC_MARQUEE,
                     onClick = {
                         onChangeTooLongTextOption(TooLongTextOption.BASIC_MARQUEE)
-                        tooLongTextOptionValue = TooLongTextOption.BASIC_MARQUEE
                     }
                 )
             }
@@ -177,10 +282,9 @@ fun LibrarySettings(
                 )
 
                 RadioButton(
-                    selected = tooLongTextOptionValue == TooLongTextOption.ELLIPSIS,
+                    selected = tooLongTextOption == TooLongTextOption.ELLIPSIS,
                     onClick = {
                         onChangeTooLongTextOption(TooLongTextOption.ELLIPSIS)
-                        tooLongTextOptionValue = TooLongTextOption.ELLIPSIS
                     }
                 )
             }
@@ -200,4 +304,10 @@ enum class TooLongTextOption {
 enum class AlreadyReadOption {
     TIMES_AND_CHECKMARK,
     CHECKMARK
+}
+
+enum class PdfReadingLayout {
+    CONTINUOUS,
+    SPREAD,
+    PAGED
 }
