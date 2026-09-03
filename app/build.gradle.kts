81a9c904d00dc5d8852687567b6aed1fa7cae498
Tulis <agma116434@gmail.com>
2026-09-03 21:35:00 +0200

    bump: version v0.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -169,8 +169,8 @@ android {
         applicationId = "dev.tulis.readbear"
         minSdk = 24
         targetSdk = 37
-        versionCode = 1
-        versionName = "v0.2.0"
+        versionCode = 2
+        versionName = "v0.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
