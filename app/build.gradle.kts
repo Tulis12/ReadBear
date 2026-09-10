6d0874f80b3d21ffc7aafd15afbb1669d9ebfe76
Tulis12 <agma116434@gmail.com>
2026-09-10 14:29:45 +0200

    bump(version): release v0.6.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,8 +59,8 @@ android {
         applicationId = "dev.tulis.readbear"
         minSdk = 24
         targetSdk = 37
-        versionCode = 12
-        versionName = "v0.6.0"
+        versionCode = 13
+        versionName = "v0.6.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
